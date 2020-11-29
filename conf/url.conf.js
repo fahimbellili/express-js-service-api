09cde468a24576ee94feed93845970067036bbb8
Fahim BELLILI <fahimbellili@gmail.com>
2020-11-29 21:43:04 +0100

    URL updates

--- conf/url.conf.js
+++ conf/url.conf.js
@@ -1,17 +1,33 @@
-export const base_url = '/api/v1';
+const base_url = '/api/v1';
 
 // User URLs
-export const user_url = '/user';
-export const user_register_url = `${user_url}/register`;
-export const user_login_url = `${user_url}/login`;
-export const user_current_url = `${user_url}/me`;
+const user_url = '/user';
+const user_register_url = `${user_url}/register`;
+const user_login_url = `${user_url}/login`;
+const user_current_url = `${user_url}/me`;
 
 // Todo URLs
-export const todo_post_url = `${user_current_url}/todo`;
-export const todo_put_del_get_one_url = `${todo_post_url}/:id`;
-export const todo_get_all_url = `${user_current_url}/all-todos`;
+const todo_post_url = `${user_current_url}/todo`;
+const todo_put_del_get_one_url = `${todo_post_url}/:id`;
+const todo_get_all_url = `${user_current_url}/all-todos`;
 
 // TodoList URLs
-export const todoList_post_url = `${user_current_url}/todo-list`;
-export const todoList_put_del_get_one_url = `${todoList_post_url}/:id`;
-export const todoList_get_all_url = `${user_current_url}/all-todo-lists`;
+const todoList_post_url = `${user_current_url}/todo-list`;
+const todoList_put_del_get_one_url = `${todoList_post_url}/:id`;
+const todoList_get_all_url = `${user_current_url}/all-todo-lists`;
+
+module.exports = {
+  base_url,
+  user_url,
+  user_register_url,
+  user_login_url,
+  user_current_url,
+
+  todo_post_url,
+  todo_put_del_get_one_url,
+  todo_get_all_url,
+
+  todoList_post_url,
+  todoList_put_del_get_one_url,
+  todoList_get_all_url,
+};
