7758688a8cb73a50fa0e795f078c8405bac49927
Fahim BELLILI <fahimbellili@gmail.com>
2020-12-16 22:31:28 +0100

    Todo List CRUD

--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const App = require('../lib/App');
 const TodoController = require('../lib/controllers/TodoController');
+const TodoListController = require('../lib/controllers/TodoListController');
 
 const UserController = require('../lib/controllers/UserController');
 
-const app = new App([new UserController(), new TodoController()]);
+const app = new App([
+  new UserController(),
+  new TodoController(),
+  new TodoListController(),
+]);
 
 app.listen();
